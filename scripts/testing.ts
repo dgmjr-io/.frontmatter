b03e866c71b06837c5502c367eba6a205e428f7f
David G. Moore, Jr <david@dgmjr.io>
2023-07-12 22:54:29 -0400

    Added some
    Added some JSON configurations for page folders and media database, and updated the testing script to include media information.

--- scripts/testing.ts
+++ scripts/testing.ts
@@ -1,4 +1,5 @@
 import { authors } from "./author";
+import { media } from "./media";
 
 for(var i = 0; i < authors.length; i++)
 {
@@ -11,5 +12,10 @@ GitHub ID: ${authors[i].github}
 Avatar: ${authors[i].avatar}
 LinkedIn: ${authors[i].linkedin}
 Initials: ${authors[i].initials}
-Slug: ${authors[i].slug}`);
+Slug: ${authors[i].slug}\r\n`);
+}
+
+for(var m in media.frontmatter)
+{
+    console.log(JSON.stringify(m));
 }
